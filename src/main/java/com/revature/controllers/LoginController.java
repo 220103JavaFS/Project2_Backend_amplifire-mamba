d42c735e1716846b991bedc039c408b9715563de
idanzigm <idanzigm@gmail.com>
2022-02-12 18:37:16 -0500

    login and logout are set up

--- src/main/java/com/revature/controllers/LoginController.java
+++ src/main/java/com/revature/controllers/LoginController.java
@@ -1,8 +1,37 @@
 package com.revature.controllers;
 
+import com.revature.models.User;
+import com.revature.models.UserDTO;
 import com.revature.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 public class LoginController {
 
     private UserService userService;
+
+    @Autowired
+    public LoginController(UserService userService) {this.userService = userService;}
+
+    @PutMapping
+    public ResponseEntity<User> getUserByUsername(@RequestBody UserDTO userDTO, HttpServletRequest request){
+        User user = userService.getUserByUsername(userDTO);
+        if(user!=null){
+            request.getSession().setAttribute("user", user.getUserId());
+            return ResponseEntity.status(200).build();
+        }
+        return ResponseEntity.status(401).build();
+    }
+
+    @RequestMapping
+    public void logout(HttpServletRequest request) {
+       HttpSession session = request.getSession();
+       session.invalidate();
+    }
 }
