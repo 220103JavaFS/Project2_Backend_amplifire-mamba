59f1b1f7001367e0376281c0b0c3a569accce791
mahadHK <themahad16@fmail.com>
2022-02-15 19:50:52 -0500

    changes

--- src/main/java/com/revature/controllers/LoginController.java
+++ src/main/java/com/revature/controllers/LoginController.java
@@ -2,7 +2,8 @@ package com.revature.controllers;
 
 import com.revature.service.UserService;
 
-public class LoginController {
+public class
+LoginController {
 
     private UserService userService;
 }
